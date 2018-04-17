f3c759ac9b774dd890a0c74b7a1b6ddd6678c052
Joy Bushnell <joy.bushnell@gmail.com>
2018-04-17 16:53:23 -0400

    RQ 3 Started
    This is the start of research question 3

---
+++ deletedPosts.py
@@ -0,0 +1,41 @@
+import csv
+
+with open('deletedPosts.csv') as deletedPosts:
+	with open('onedayfliesId.csv') as fliesId:
+		with open('notOneDayFliesId.csv') as NotId:
+			fliesReader = csv.reader(fliesId)
+			NotReader = csv.reader(NotId)
+			deletedReader = csv.reader(deletedPosts)
+
+			count = 0
+			rowcount = 0
+			deletedIds =[]
+			for row in deletedReader:
+				if 'Id' not in row:
+					deletedIds.append(float(row[0]))
+			#print(deletedIds)
+
+			for row in fliesReader:
+				
+				if 'id' not in row[0]:
+					rowcount += 1
+					if float(row[0]) in deletedIds:
+						count += 1
+
+			print("total number of deleted= "+str(count))	
+			print("total number = "+str(rowcount))		
+					
+			count = 0
+			rowcount2 = 0
+			for row in NotReader:
+				rowcount2 += 1
+				if 'id' not in row[0]:
+					rowcount += 1
+					if float(row[0]) in deletedIds:
+						count += 1
+				if rowcount2 >= rowcount:
+					break
+				print(rowcount2)
+			print("total number of deleted for others= "+str(count))	
+			print("total number = "+str(rowcount2))		
+					
