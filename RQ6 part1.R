b36954f0fc8fba85cb2282297793f0f9694886db
Tota <35812950+haifaaldossari@users.noreply.github.com>
2018-04-17 16:30:48 -0400

    RQ6 part 1

---
+++ RQ6 part1.R
@@ -0,0 +1,11 @@
+#RQ6
+
+#import files
+allUsersPostedQuestions <- read_csv("Downloads/allUsersPostedQuestions.csv")
+onedayflies_user_ids <- read_csv("Downloads/onedayflies_user_ids.csv")
+
+#use the match funcation called semi_join
+ match1 <- semi_join(onedayflies_user_ids, allUsersPostedQuestions)
+ 
+ # total number of onedayflies that are not onedayflies anymore is 436 out of the 128260
+ 
